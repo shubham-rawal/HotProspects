0489142924e805943259f5955781568c1a3a7384
shubham-rawal <shubhamrawal2001@gmail.com>
2022-09-06 16:21:11 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Shubham Rawal on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
