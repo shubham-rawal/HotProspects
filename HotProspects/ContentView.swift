5b81d594287499e2348031c6b77b5cfe77cab532
shubham-rawal <shubhamrawal2001@gmail.com>
2022-09-14 12:58:40 +0530

    basic tabView layout for app

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,11 +6,32 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            ProspectsView()
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView()
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView()
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
