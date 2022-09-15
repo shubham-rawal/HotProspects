652340129f118c710f02cd6de1cca6fc4dc806c1
shubham-rawal <shubhamrawal2001@gmail.com>
2022-09-15 17:32:47 +0530

    Package added for scanning code, ImageSaver added for saving qr code to Photos, notifications for Reminder.

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -10,8 +10,9 @@ import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
-    @State private var name = "Anonymous"
-    @State private var emailAddress = "you@yoursite.com"
+    @State private var name = ""
+    @State private var emailAddress = ""
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -19,22 +20,50 @@ struct MeView: View {
     var body: some View {
         NavigationView {
             Form {
-                TextField("Name", text: $name)
-                    .textContentType(.name)
-                    .font(.title)
+                Section{
+                    TextField("Name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                    
+                    TextField("Email Address", text: $emailAddress)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                        .keyboardType(.emailAddress)
+                        .textCase(.lowercase)
+                }
                 
-                TextField("Email Address", text: $emailAddress)
-                    .textContentType(.emailAddress)
-                    .font(.title)
-                
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
-                    .resizable()
-                    .interpolation(.none)
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
+                Section("Your QR Code") {
+                    HStack {
+                        Spacer()
+                        
+                        Image(uiImage: qrCode)
+                            .resizable()
+                            .interpolation(.none)
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .contextMenu {
+                                Button {
+                                    //save to photo library
+                                    let imageSaver = ImageSaver()
+                                    imageSaver.writeToPhotoAlbum(image: qrCode)
+                                } label: {
+                                    Label("Save to Photos", systemImage: "square.and.arrow.down")
+                                }
+                            }
+                        
+                        Spacer()
+                    }
+                }
             }
             .navigationTitle("Your Code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Shubham Rawal on 14/09/22.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -15,6 +16,7 @@ struct ProspectsView: View {
     
     //this will find the Prospects class in the environment, attach it to the local prospects property, watch it for changes and re-invoke the body property when there is a change.
     @EnvironmentObject var prospects : Prospects
+    @State private var isShowingScanner = false
     var filter: FilterType
     
     var title : String {
@@ -50,19 +52,95 @@ struct ProspectsView: View {
                         Text(prospect.emailAddress)
                             .foregroundColor(.secondary)
                     }
+                    .swipeActions {
+                        if prospect.isContacted {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.red)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                            
+                            Button {
+                                addNotification(for: prospect)
+                            } label: {
+                                Label("Remind Me!", systemImage: "bell")
+                            }
+                            .tint(.orange)
+                        }
+                    }
                 }
             }
             .navigationTitle(title)
             .toolbar {
                 Button {
-                    let prospect = Prospect()
-                    prospect.name = "Shubham Rawal"
-                    prospect.emailAddress = "shubhamrawal2001@gmail.com"
-                    prospects.people.append(prospect)
+                    isShowingScanner = true
                 } label: {
                     Label("Scan", systemImage: "qrcode.viewfinder")
                 }
             }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Shubham Rawal\nshubhamrawal2001@gmail.com", completion: handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        //more code to come
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.add(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect){
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content =  UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)!"
+            content.subtitle = prospect.emailAddress
+            content.sound = .default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger )
+            center.add(request)
+        }
+        
+        //more code to come
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Request Denied.")
+                    }
+                }
+            }
+            
         }
     }
 }
